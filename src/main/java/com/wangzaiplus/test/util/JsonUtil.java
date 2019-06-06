7fb5e0de421d8fdf35b4b4ddc862261203581860
wenxingwang <18621142249@163.com>
2019-06-06 10:28:57 +0800

    optimize JsonUtil

--- src/main/java/com/wangzaiplus/test/util/JsonUtil.java
+++ src/main/java/com/wangzaiplus/test/util/JsonUtil.java
@@ -1,6 +1,7 @@
 package com.wangzaiplus.test.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -12,20 +13,20 @@ import java.text.SimpleDateFormat;
 @Slf4j
 public class JsonUtil {
 
-    private static ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper objectMapper = new ObjectMapper();
     private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     static {
         // 对象的所有字段全部列入
-        mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
         // 取消默认转换timestamps形式
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         // 忽略空bean转json的错误
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         // 统一日期格式
-        mapper.setDateFormat(new SimpleDateFormat(DATE_FORMAT));
+        objectMapper.setDateFormat(new SimpleDateFormat(DATE_FORMAT));
         // 忽略在json字符串中存在, 但在java对象中不存在对应属性的情况, 防止错误
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
     public static <T> String objToStr(T obj) {
@@ -34,7 +35,7 @@ public class JsonUtil {
         }
 
         try {
-            return obj instanceof String ? (String) obj : mapper.writeValueAsString(obj);
+            return obj instanceof String ? (String) obj : objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
             log.warn("objToStr error: ", e);
             return null;
@@ -47,9 +48,22 @@ public class JsonUtil {
         }
 
         try {
-            return clazz.equals(String.class) ? (T) str : mapper.readValue(str, clazz);
+            return clazz.equals(String.class) ? (T) str : objectMapper.readValue(str, clazz);
         } catch (Exception e) {
             log.warn("strToObj error: ", e);
+            return null;
+        }
+    }
+
+    public static <T> T strToObj(String str, TypeReference<T> typeReference) {
+        if (StringUtils.isBlank(str) || null == typeReference) {
+            return null;
+        }
+
+        try {
+            return (T) (typeReference.getType().equals(String.class) ? str : objectMapper.readValue(str, typeReference));
+        } catch (Exception e) {
+            log.error("strToObj error", e);
             return null;
         }
     }
