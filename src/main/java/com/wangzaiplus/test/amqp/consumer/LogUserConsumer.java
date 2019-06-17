b3134a3d37be35f509b17a22ff1a8fd6aaaf841e
wenxingwang <18621142249@163.com>
2019-06-17 14:06:17 +0800

    test msg confirms and return callback

--- src/main/java/com/wangzaiplus/test/amqp/consumer/LogUserConsumer.java
+++ src/main/java/com/wangzaiplus/test/amqp/consumer/LogUserConsumer.java
@@ -1,11 +1,16 @@
 package com.wangzaiplus.test.amqp.consumer;
 
+import com.rabbitmq.client.Channel;
 import com.wangzaiplus.test.pojo.UserLog;
 import com.wangzaiplus.test.service.UserLogService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @Slf4j
@@ -15,9 +20,16 @@ public class LogUserConsumer {
     UserLogService userLogService;
 
     @RabbitListener(queues = "log.user.queue")
-    public void logUserConsumer(UserLog userLog) {
-        log.info("消费者1消费消息: {}", userLog.toString());
-        userLogService.insert(userLog);
+    public void logUserConsumer(UserLog userLog, Channel channel, @Header (AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
+        try {
+            log.info("收到消息: {}", userLog.toString());
+            userLogService.insert(userLog);
+        } catch (Exception e){
+            log.error("logUserConsumer error", e);
+            channel.basicNack(tag, false, true);
+        } finally {
+            channel.basicAck(tag, false);
+        }
     }
 
 }
