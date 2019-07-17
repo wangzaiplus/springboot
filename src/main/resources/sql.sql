cee44fc72320a904248a3c6df6e7003d5349f78a
wenxingwang <18621142249@163.com>
2019-07-17 16:35:52 +0800

    sql

---
+++ src/main/resources/sql.sql
@@ -0,0 +1,32 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unq_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `login_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL COMMENT '用户id',
+  `type` int(11) NOT NULL COMMENT '日志类型:1登录 2登出',
+  `description` varchar(255) DEFAULT NULL COMMENT '日志描述',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2045 DEFAULT CHARSET=utf8mb4 COMMENT='登录日志表';
+
+CREATE TABLE `msg_log` (
+  `msg_id` varchar(255) NOT NULL DEFAULT '' COMMENT '消息唯一标识',
+  `msg` text COMMENT '消息体, json格式化',
+  `exchange` varchar(255) NOT NULL DEFAULT '' COMMENT '交换机',
+  `routing_key` varchar(255) NOT NULL DEFAULT '' COMMENT '路由键',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '状态: 0投递中 1投递成功 2投递失败',
+  `try_count` int(11) NOT NULL DEFAULT '0' COMMENT '重试次数',
+  `next_try_time` datetime DEFAULT NULL COMMENT '下一次重试时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`msg_id`),
+  UNIQUE KEY `unq_msg_id` (`msg_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='消息投递日志';
+
