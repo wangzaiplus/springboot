fee41195fdbf6d29ee7329b9e9bd5e83b58ec6b0
wangzaiplus <18621142249@163.com>
2020-05-24 18:40:33 +0800

    rabbitmq

--- src/main/java/com/wangzaiplus/test/config/RabbitConfig.java
+++ src/main/java/com/wangzaiplus/test/config/RabbitConfig.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @Slf4j
 public class RabbitConfig {
 
